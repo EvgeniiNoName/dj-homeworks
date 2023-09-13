39de85d2c8b28edd8fbb24986216c16f36ab25fe
EvgeniiNoName <125493179+EvgeniiNoName@users.noreply.github.com>
2023-09-14 00:08:22 +0700

    second

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -40,13 +40,11 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         request = self.context['request']
         """Метод для валидации. Вызывается при создании и обновлении."""
         if data.get('status') == 'OPEN' or request.method == "POST":
-            count = 0
-            for adv in request.user.advertisements.all():
-                if adv.status == "OPEN":
-                    count += 1
+            count = request.user.advertisements.filter(status="OPEN").count()
             if count > 10:
                 raise ValidationError(
                     'Can\'t create more than 10 open posts, '
                     'delete or close some of your posts.'
                 )
         return data
+
